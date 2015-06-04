97b84f7f4426f871734f90b68371588011d0581a
Dev <dev.rangarajan@gmail.com>
2015-06-04 14:32:47 -0700

    More Changes.

---
+++ KarateChop1.py
@@ -0,0 +1,12 @@
+#Karate Chop v1
+array = [range(1, 101)]
+print array
+
+def chop1 (target, array):
+	for i in array(i):
+		print "value", i
+		if i == target:
+			print 'true'
+		else:
+			print 'false'
+chop1(48, array)
